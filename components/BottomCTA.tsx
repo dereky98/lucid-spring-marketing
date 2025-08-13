0846d05174296eb6f996e52f35adcaf2f6c39daa
Derek Ye <derekye.98@gmail.com>
2025-08-13 15:26:01 -0700

    Fix BottomCTA on mobile

--- components/BottomCTA.tsx
+++ components/BottomCTA.tsx
@@ -4,7 +4,10 @@ export default function BottomCTA() {
   return (
     <section className="relative bg-[#272727] text-white pt-20 sm:pt-56 md:pt-100 pb-8 sm:pb-12 px-6">
       {/* Decorative oversized brand text in bottom-left */}
-      <div aria-hidden className="pointer-events-none select-none absolute left-2 bottom-2 z-0">
+      <div
+        aria-hidden
+        className="pointer-events-none select-none absolute left-1/2 sm:left-2 -translate-x-1/2 sm:translate-x-0 bottom-2 z-0"
+      >
         <span className="font-playfair leading-none text-white text-[16vw] sm:text-[18vw] md:text-[20vw]">
           Palace
         </span>
